e51633a80c6ac1884bf1f335f0a1aae128567c27
Rishabh Jha <rishabhjha708@gmail.com>
2020-05-12 06:27:51 +0530

    test setup vscode and configs

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    cin >> a;
+    printf("%d", a);
+    cout << "hello";
+    return 0;
+}
+
+
