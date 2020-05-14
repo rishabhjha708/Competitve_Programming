1a868305b817629295b7b2c0310149a8633a5ecd
Rishabh Jha <rishabhjha708@gmail.com>
2020-05-15 01:46:09 +0530

    ProjectEuler #1

--- test.cpp
+++ test.cpp
@@ -7,10 +7,13 @@ using namespace __gnu_pbds;
 
 int main(int argc, char const *argv[])
 {
-    int a;
-    cin >> a;
-    printf("%d", a);
-    cout << "hello";
+    int a = 1000;
+    // sum of multiple of 3 below a
+    int sum3 = 3 * (((a-1)/3 * (((a-1)/3) + 1))/2);
+    int sum5 = 5 * (((a-1)/5 * (((a-1)/5) + 1))/2);
+    int sum15 = 15 * (((a-1)/15 * (((a-1)/15) + 1))/2);
+    printf("%d %d\n", sum3, sum5);
+    printf("%d\n", sum3 + sum5 - sum15);
     return 0;
 }
 
